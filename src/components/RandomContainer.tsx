33f37a9a996f7bcf6ab8f38279c3dd8aeab0bd1e
raymoo42 <raymoo42@users.noreply.github.com>
2023-09-30 20:26:55 +0200

    add more job icons

--- src/components/RandomContainer.tsx
+++ src/components/RandomContainer.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import JobIconDisplay from "./JobIconDisplay";
 
 export default function RandomContainer() {
-
   const [jobs, setJobs] = useState([0,0,0,0]);
   
   const randomize = () => {
@@ -11,13 +10,13 @@ export default function RandomContainer() {
 
   return (
     <div className="grid grid-cols-4 gap-2">
-      <div className="text-sky-400 text-center bg-zinc-900 rounded-xl py-6">
-        Job 1
-        <JobIconDisplay/>
-      </div>
-      <div className="text-green-400 text-center">Job 2</div>
-      <div className="text-red-400 text-center">Job 3</div>
-      <div className="text-red-400 text-center">Job 4</div>
+
+      {jobs.map(job => 
+        <div className="text-sky-400 text-center bg-zinc-900 rounded-xl py-6">
+            Job 1
+          <JobIconDisplay/>
+        </div>
+        )}
 
       <button
       onClick={randomize} 
